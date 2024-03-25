53bea27fa946bbebf7d81d22faf9637b0882503f
plykung <58039748+plykung@users.noreply.github.com>
2024-03-25 23:33:24 +0700

    fix mainpage to react-daisyui

--- app/components/special/MobileNotFullAlert.tsx
+++ app/components/special/MobileNotFullAlert.tsx
@@ -1,14 +1,12 @@
+"use client"
+
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import { Alert } from "react-daisyui";
 
 export default function MobileNotFullAlert() {
-    return (<>
-        <div role="alert" className="alert alert-info">
-            <div>
-                <ExclamationTriangleIcon className="stroke-current shrink-0 w-6 h-6" />
-            </div>
-
+    return (
+        <Alert status="info" icon={<ExclamationTriangleIcon className="stroke-current shrink-0 w-6 h-6" />}>
             <span>GoExam are not fully supported on mobile yet. Functionalities may be limited.</span>
-
-        </div>
-    </>)
+        </Alert>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
+"use client"
+
 import { PlusCircleIcon, StarIcon } from "@heroicons/react/24/outline"
 import Image from "next/image"
 import Link from "next/link"
+import { Button, Hero } from "react-daisyui"
 
 export default function Home() {
   return (
 
     <>
-      <div className="hero min-h-screen bg-base-200 px-12">
-        <div className="hero-content justify-between flex-col md:flex-row-reverse w-[100%]">
+      <div className="hero min-h-screen bg-base-300 px-12">
+
+        <Hero >
+          <Hero.Content className="w-[100%] text-start justify-between flex-col md:flex-row-reverse max-md:space-y-4">
+            <Image src="https://placehold.co/400/png" width={400} height={400} alt="GoExam Logo" className="z-[0]" />
+            <div className="py-5">
+              <h1 className="text-5xl font-bold">GoExam</h1>
+              <p className="py-6">
+                Practice Exams Generator. <br />
+                For Better Score, Better Future.
+              </p>
+              <Link href="https://github.com/plykung/goexam-remake" target="_blank">
+                <Button variant="outline" color="secondary" className="mx-2 px-14 py-1 truncate "><StarIcon className="w-4 h-4 pe-1" /> Follow us on Github</Button>
+              </Link>
+            </div>
+          </Hero.Content>
+        </Hero>
+
+        {/* <div className="hero-content justify-between flex-col md:flex-row-reverse w-[100%]">
 
           <div className="z=[0]">
             <Image src="https://placehold.co/400/png" width={400} height={400} alt="hero" />
@@ -18,25 +38,25 @@ export default function Home() {
             <p className="pt-6">Practice exams generator. For Better Score, Better Future.</p>
             <p className="pb-6">Coming Soon!</p>
             <div className="flex">
-              {/* <Link href="/create">
+               <Link href="/create">
                 <button className="btn btn-primary mx-2 px-14">New Exam</button>
               </Link>
               <Link href="/exercise">
                 <button className="btn btn-outline btn-secondary mx-2 px-14">Your Exam</button>
-              </Link> */}
-
-              <Link href="https://github.com/plykung/goexam-remake" target="_blank">
-                <button className="btn btn-outline btn-secondary mx-2 px-14"><StarIcon className="w-4 h-4 pe-1" /> Follow us on Github</button>
               </Link>
 
+        <Link href="https://github.com/plykung/goexam-remake" target="_blank">
+          <button className="btn btn-outline btn-secondary mx-2 px-14"><StarIcon className="w-4 h-4 pe-1" /> Follow us on Github</button>
+        </Link>
 
-            </div>
 
-
-          </div>
-
-        </div>
       </div>
+
+
+    </div >
+
+        </div > */}
+      </div >
       <div className="px-5">
 
       </div>
